a42453d4b6d9df9fd72618eb49b2e8962d18645b
akhalitov <36495901+akhalitov@users.noreply.github.com>
2018-03-05 15:24:42 +0800

    no message

--- scripts/company.php
+++ scripts/company.php
@@ -54,9 +54,6 @@
         public $password;
 
 		
-		// MyWeatherDemo API URL to work with companies
-		const BASE_URL = "http://www.myweatherdemo.com/api/company/";
-
 		public function provision(){
             
             // to create a company in external service we need to pass country, city and name of the company
@@ -67,7 +64,8 @@
                     'name' => $this->account->companyName
             );
             
-            $response = $this->send_curl_request('POST', self::BASE_URL, $request);
+            $url = $this->application->url . "company/";
+            $response = $this->send_curl_request('POST', $url, $request);
             // need to save company_id in APSC, going to use that later to delete a resource in unprovision()
             // username and password will be used to login to MyWeatherDemo web interface
             $this->company_id = $response->{'id'};
@@ -78,13 +76,13 @@
         // unprovision() method is executed when resource is removed (DELETE)
         public function unprovision(){
             // need to pass company_id to indicate which company we want to delete
-            $url = self::BASE_URL . $this->company_id;
+            $url = $this->application->url . "company/" . $this->company_id;
             $response = $this->send_curl_request('DELETE', $url);
         }
 
 		// you can add your own methods as well, don't forget to make them private
 		private function send_curl_request($verb, $url, $payload = ''){
-			$token = "osamwd";
+			$token = $this->application->token;
 			$headers = array(
 					'Content-type: application/json',
 					'x-provider-token: '. $token
